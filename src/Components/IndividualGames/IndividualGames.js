d9656698b0b38be31c9682d51112c30cb6f868c9
Yiqi Zhang <>
2020-05-21 16:59:35 -0400

    individual games  toggle done

--- src/Components/IndividualGames/IndividualGames.js
+++ src/Components/IndividualGames/IndividualGames.js
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-// Helpers
-// import { headerIcons } from "../../../Helpers/Icons";
-
-// Styles
-// import {
-//   HeaderContainerDiv,
-//   HeaderIconDiv,
-//   HeaderIconImage,
-// } from "./HeaderBar.styles.js";
+import { IndividualGamesContainer } from "./IndividualGames.styles.js";
 
 const IndividualGames = () => {
+  const [season, setSeason] = useState(2);
+
+  const handleSeasonSubmit = (event) => {
+    if (season === 2) {
+      setSeason(1);
+      event.preventDefault();
+    }
+    if (season === 1) {
+      setSeason(2);
+      event.preventDefault();
+    }
+  };
+
   return (
-    <div>bye</div>
-    // <HeaderContainerDiv>
-    //   <HeaderIconDiv>
-    //     <HeaderIconImage src={headerIcons[0]} alt="" />
-    //     <HeaderIconImage
-    //       style={{ maxWidth: "none" }}
-    //       src={headerIcons[1]}
-    //       alt=""
-    //     />
-    //     <HeaderIconImage
-    //       style={{ padding: "0.4em 0.3em 0 0" }}
-    //       src={headerIcons[2]}
-    //       alt=""
-    //     />
-    //   </HeaderIconDiv>
-    // </HeaderContainerDiv>
+    <IndividualGamesContainer>
+      <>
+        <div>
+          <form onSubmit={handleSeasonSubmit}>
+            <button style={{ color: "white" }}>Toggle Season</button>
+          </form>
+        </div>
+        {season === 2 ? (
+          <>
+            <h2>Season Two</h2>
+            <h3>
+              Yiqi: 1 <br></br>
+              Mickias: 1 <br></br>
+              Rob:0 <br></br>
+              Dylan:1 <br></br>
+            </h3>
+          </>
+        ) : (
+          <>
+            <h2>Season One</h2>
+            <h3>**Advanced analytics not available</h3>
+            <h3>
+              Yiqi: 14 <br></br>
+              Mickias: 11 <br></br>
+              Rob:11 <br></br>
+              Dylan: 6 <br></br>
+              Gray: 1 <br></br>
+              Jacqueline: 1
+            </h3>
+          </>
+        )}
+      </>
+    </IndividualGamesContainer>
   );
 };
 
---
+++ src/Components/IndividualGames/IndividualGames.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const IndividualGamesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
