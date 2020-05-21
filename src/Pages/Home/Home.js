fab43b7dae90cae9fbd0b9f8e47daf6241186055
Yiqi Zhang <>
2020-05-21 15:23:16 -0400

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Styles
+import { OutterContainerDiv } from "./Home.styles";
+
+const Home = () => {
+  return (
+    <>
+      <OutterContainerDiv>
+        <div>Hi</div>
+      </OutterContainerDiv>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Home/Home.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const OutterContainerDiv = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: #000000;
+  color: white;
+`;
